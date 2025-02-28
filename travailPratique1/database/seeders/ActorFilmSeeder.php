f3e38f07b1e560e1f5a0901d807e053f466b6c36
Crashtomzy <thomasplourde12@gmail.com>
2025-02-27 20:35:06 -0500

    creation seeders

---
+++ travailPratique1/database/seeders/ActorFilmSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ActorFilmSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sql = file_get_contents(database_path() . '/seeders/actor_film.sql');
+        DB::statement($sql);
+    }
+}
---
+++ travailPratique1/database/seeders/FilmSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class FilmSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $sql = file_get_contents(database_path() . '/seeders/films.sql');
+        DB::statement($sql);
+    }
+}
