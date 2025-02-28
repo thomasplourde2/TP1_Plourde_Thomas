8b92f989391d5a69c31cc73077df11516cced770
Crashtomzy <thomasplourde12@gmail.com>
2025-02-27 21:54:20 -0500

    creation fakers

--- travailPratique1/database/factories/UserFactory.php
+++ travailPratique1/database/factories/UserFactory.php
@@ -5,6 +5,7 @@ namespace Database\Factories;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
+use Faker\Factory as Faker;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
@@ -23,12 +24,13 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = Faker::create();
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'login' =>$faker->userName,
+            'password' =>$faker->password,
+            'email' =>$faker->email,
+            'last_name' =>$faker->lastName,
+            'first_name' =>$faker->firstName
         ];
     }
 
