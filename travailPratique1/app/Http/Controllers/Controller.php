36280455f9abcf5fc13c013d2b1a683af778c713
Crashtomzy <thomasplourde12@gmail.com>
2025-03-05 09:37:46 -0500

    creation filmcontroller

--- travailPratique1/app/Http/Controllers/Controller.php
+++ travailPratique1/app/Http/Controllers/Controller.php
@@ -2,6 +2,12 @@
 
 namespace App\Http\Controllers;
 
+define('INVALID_DATA', 422);
+define('NOT_FOUND', 404);
+define('SERVER_ERROR', 500);
+define('TABLE_CREATED', 201);
+define('OK', 200);
+
 abstract class Controller
 {
     //
